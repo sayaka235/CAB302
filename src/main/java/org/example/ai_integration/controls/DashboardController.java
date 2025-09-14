bc6d8dbfd0c61c55250e4fbb456b987a31fc4218
Layla Ahern <layla.ahern@connect.qut.edu.au>
2025-09-14 23:07:10 +1000

    made dashboard a bit nicer

--- src/main/java/org/example/ai_integration/controls/DashboardController.java
+++ src/main/java/org/example/ai_integration/controls/DashboardController.java
@@ -4,15 +4,26 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import org.example.ai_integration.Navigator;
 import org.example.ai_integration.model.UserManager;
 
 import javax.swing.*;
+import java.util.Objects;
 
 public class DashboardController {
+    @FXML private ImageView Stars;
     @FXML private Label WelcomeLabel;
     public void initialize(){
         WelcomeLabel.setText("Hello " + UserManager.getInstance().getLoggedInUser().getName());
+        try {
+            // Assuming "myImage.png" is in src/main/resources/images
+            Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/stars.png")));
+            Stars.setImage(image);
+        } catch (Exception e) {
+            System.err.println("Error loading image: " + e.getMessage());
+        }
     }
 
     @FXML
